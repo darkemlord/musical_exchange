7812582331b31a34a869031e4900480e896fbbd8
emanuel caro <darkemlord@gmail.com>
2022-01-21 21:53:27 +0900

    product show page created

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,10 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   private
   def products_params
     params.require(:product).permit(:user_id)
